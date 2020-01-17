0e165fa5dbaf64c9346254f79e2380fdd623230f
Patrick Assoa Adou <kanian77@gmail.com>
2020-01-17 00:42:28 +0000

    refactor: Refactor RealEither

--- src/Of.ts
+++ src/Of.ts
@@ -1,12 +1,10 @@
 class Of<T> {
-    //public $value
     constructor(protected $value: T) {
-        this.$value = $value
+        //this.$value = $value
     }
     public static of<T>(x: T): Of<T> {
         return new Of<T>(x)
     }
-
     public get value(){
         return this.$value
     }
